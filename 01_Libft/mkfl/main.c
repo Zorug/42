a7be8fcff12c7a08295c6e6b507ba6af7f347d36
Cassiano Gross Schuler <cgross-s@c1r5s5.42porto.com>
2024-10-21 22:55:22 +0100

    updating_make_and_name

---
+++ 01_Libft/mkfl/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main(void)
+{
+    printf("Iniciando o programa...\n");
+    print_mensagem();
+    return 0;
+}
---
+++ 01_Libft/mkfl/utils.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_mensagem(void)
+{
+    printf("Olá do arquivo utils.c!\n");
+}
