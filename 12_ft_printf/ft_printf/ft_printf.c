186dab4fcdcbe11254d8ce5b8a506cedb86e1a4e
Zorug <cassiano_gross@hotmail.com>
2024-12-09 22:37:38 +0000

    starting printf

---
+++ 12_ft_printf/ft_printf/ft_printf.c
@@ -0,0 +1,11 @@
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+
+}
+
+int main()
+{
+	ft_printf();
+}
---
+++ 12_ft_printf/ft_printf/ft_printf.h
@@ -0,0 +1,8 @@
+#ifdef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+#endif
