29c1c7a2d0833e5d89fb715a9bdbfc237df39341
Zorug <cassiano_gross@hotmail.com>
2025-01-04 22:28:14 +0000

    updates

--- 13_get_next_line/main/get_next_line.c
+++ 13_get_next_line/main/get_next_line.c
@@ -1,5 +1,35 @@
 #include "get_next_line.h"
 
+/*Polish linked list for next call*/
+void	polish_list(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	int		i;
+	int		k;
+	char	*buf;
+
+	buf = malloc(BUFFER_SIZE + 1);
+	clean_node = malloc(sizeof(t_list));
+	if (NULL == buf || NULL == clean_node)
+		return ;
+	last_node = find_last_node(*list);
+
+	i = 0;
+	k = 0;
+	while (last_node->str_buf[i] != '\0'
+		&& last_node->str_buf[i] != '\n')
+		++i;
+	//		is it != '\0'? -> '\n'
+	while (last_node->str_buf[i] != '\0'
+		&& last_node->str_buf[++i])
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	dealloc(list, clean_node, buf);
+}
+
 /*append one node to the end of list*/
 void	append(t_list **list, char *buf)
 {
@@ -20,7 +50,26 @@ void	append(t_list **list, char *buf)
 	new_node->str_buf = buf;
 	new_node->next = NULL;
 }
+/*Get my (line\n)*/
+char	*get_line(t_list *list)
+{
+	int		str_len;
+	char	*next_str;
 
+	if (NULL == list)
+		return (NULL);
+
+	// Count how many char are till '\n'
+	// So i know how much to malloc
+	str_len = len_to_newline(list);
+	next_str = malloc(str_len + 1);
+	if (NULL == next_str)
+		return (NULL);
+
+	//Copy the string in the buffer and return it 
+	copy_str(list, next_str);
+	return (next_str);
+}
 
 void	create_list(t_list **list, int fd)
 {
---
+++ 13_get_next_line/main/get_next_line_utils.c
@@ -0,0 +1,53 @@
+int len_to_newline(t_list *list)
+{
+	int	i;
+	int	len;
+
+	if (NULL == list)
+		return (0);
+	len = 0;
+	//list != NULL
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+
+			if (list->str_buf[i] == '\n')
+			{
+				++len;
+				return (len);
+			}
+			++i;
+			++len;
+		}
+		list = list->next;
+	}
+	return (len);
+}
+
+void	copy_str(t_list *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (NULL == list)
+		return;
+	k == 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	str[k] = '\0';
+}
