6f289a16eb0d419424336ac0387cda5527be9fe4
Zorug <cassiano_gross@hotmail.com>
2025-04-24 12:02:57 +0100

    tests

--- 23_pipex/main/src/main.c
+++ 23_pipex/main/src/main.c
@@ -6,7 +6,7 @@
 /*   By: cgross-s <cgross-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 21:54:59 by cgross-s          #+#    #+#             */
-/*   Updated: 2025/04/22 21:58:43 by cgross-s         ###   ########.fr       */
+/*   Updated: 2025/04/24 12:01:47 by cgross-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,3 +58,44 @@ int	main(int argc, char **argv, char **envp)
 	ft_cleanup(&pipex);
 	return (0);
 }
+
+/*
+echo -e "banana\nlaranja\nbanana\nuva\nabacaxi\nbanana" > input.txt
+
+./pipex input.txt "grep banana" "wc -l" output.txt
+< input.txt grep banana | wc -l > output.txt
+
+./pipex input.txt "cat -e" "cut -d\$ -f1" output.txt
+./pipex input.txt "tr a-z A-Z" "rev" output.txt
+./pipex input.txt "grep ^b" "sort -r" output.txt
+
+./pipex input.txt "banana123" "wc -l" output.txt
+# Esperado: erro de "command not found" e exit code 127
+
+./pipex nofile.txt "cat" "wc -l" output.txt
+# Esperado: erro de "No such file or directory"
+
+touch secret.txt && chmod 000 secret.txt
+./pipex secret.txt "cat" "wc -l" output.txt
+# Esperado: "Permission denied"
+
+./pipex input.txt "grep nadaencontrado" "wc -l" output.txt
+# Esperado: output.txt deve conter 0
+
+SCRIPT .SH
+echo -e '#!/bin/bash\necho "olá do script!"' > test.sh
+chmod +x test.sh
+./pipex input.txt "./test.sh" "wc -l" output.txt
+
+./pipex input.txt "awk '{print $1}'" "sort" output.txt
+./pipex input.txt "sed 's/banana/laranja/g'" "tee log.txt" output.txt
+
+URANDOM
+./pipex /dev/urandom "head -c 100" "wc -c" output.txt
+
+Comparação automática com shell (bash vs pipex)
+< input.txt grep banana | wc -l > expected.txt
+./pipex input.txt "grep banana" "wc -l" actual.txt
+diff expected.txt actual.txt && echo "✅ OK" || echo "❌ Diferente"
+
+*/
