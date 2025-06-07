cd83069c40949945b6269c08265b8a097d3297a5
Zorug <cassiano_gross@hotmail.com>
2025-06-07 21:15:51 +0100

    adeded

--- 32_exam_03/ft_printf/test1/ft_printf.c
+++ 32_exam_03/ft_printf/test1/ft_printf.c
@@ -1,17 +1,18 @@
-#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int ft_putchar(char c)
 {
 	return write(1, &c, 1);
 }
 
-int ft_putstr(char * s) // tá certo isso?
+int ft_putstr(char *s)
 {
 	int count = 0;
+
 	if (!s)
-		s = "(null)"; // confere?
+		s = "(null)";
 	while (*s)
 	{
 		count += ft_putchar(*s);
@@ -23,13 +24,13 @@ int ft_putstr(char * s) // tá certo isso?
 int ft_putnbr(long n)
 {
 	int count = 0;
-	if (count < 0)
+	if (n < 0)
 	{
 		count += ft_putchar('-');
 		n = -n;
 	}
 	if (n >= 10)
-		count += ft_putnbr(n / 10);
+		count += ft_putnbr(n /10);
 	count += ft_putchar(n % 10 + '0');
 	return count;
 }
@@ -38,37 +39,40 @@ int ft_puthex(unsigned int n)
 {
 	int count = 0;
 	char *hex = "0123456789abcdef";
+
 	if (n >= 16)
 		count += ft_puthex(n / 16);
 	count += ft_putchar(hex[n % 16]);
 	return count;
 }
 
-int ft_printf(const char *format, ...) // const char?
+int ft_printf(char *format, ...)
 {
-	count = 0;
+	int count = 0;
 	va_list ap;
 	va_start(ap, format);
 
 	while (*format)
 	{
+
 		if (*format == '%')
 		{
-			format++;
+			format ++;
 			if (*format == 's')
-				count += ft_putchar(va_arg(ap, char *));
-			else if (*format == 'i')
+				count += ft_putstr(va_arg(ap, char *)); //arg ou args?? está certo o formato??
+			else if (*format == 'd')
 				count += ft_putnbr(va_arg(ap, int));
 			else if (*format == 'x')
 				count += ft_puthex(va_arg(ap, unsigned int));
 			else if (*format)
-				count += ft_putchar(*format);
+				count += ft_putchar(*format);	
 		}
 		else
+		{
 			count += ft_putchar(*format);
-		format++;
+		}
+		format ++;
 	}
-
 	va_end(ap);
 	return count;
 }
